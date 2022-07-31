eea9411a96375b05af5e7288c11f5c9550bd3f53
sharnobyl <psn.sharjil@gmail.com>
2022-07-31 15:22:12 +1200

    molina's code

--- frontend/src/components/Map/InteractiveMap/index.tsx
+++ frontend/src/components/Map/InteractiveMap/index.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
-import useGeolocation from "react-hook-geolocation";
-import { IPlot } from "../../../types/schema";
+import { IPlot, IPerson } from "../../../types/schema";
 import averageCoordinates from "./utils/averageCoordinates";
+import getAnniversaryPlots from "./utils/getAnniversaryPlots";
+import { getCookie } from "typescript-cookie";
+import { dateToString } from "../../../utils/dates";
 
 interface InteractiveMapProps {
   plots: IPlot[];
@@ -21,6 +23,15 @@ const InteractiveMap: React.FC<InteractiveMapProps> = ({
   const mapRef = useRef<HTMLDivElement>(null);
   const [map, setMap] = useState<google.maps.Map>();
 
+  const darkMode = getCookie("darkMode");
+  // const [cookies, setCookie] = useCookies(["user"]);
+
+  // // Memorise map instance
+  // // initialise mapId
+  const mapId: string =
+    darkMode == "true" ? "9405af4022ebaa6" : "22722d672fb630c2";
+  const plotColour: string = darkMode == "true" ? "#428BCA" : "#F0F26B";
+
   // Memorise map instance
   useEffect(() => {
     if (mapRef.current) {
@@ -39,11 +50,29 @@ const InteractiveMap: React.FC<InteractiveMapProps> = ({
           },
           disableDefaultUI: true,
           zoomControl: false,
-          mapId: "c1b071c4df766122",
+          mapTypeControl: true,
+          mapTypeControlOptions: {
+            mapTypeIds: ["roadmap", "satellite"],
+          },
+          mapId: mapId,
         })
       );
     }
-  }, [mapRef]);
+  }, [mapRef, mapId]);
+
+  function handleLocationError(
+    browserHasGeolocation: boolean,
+    infoWindow: google.maps.InfoWindow,
+    pos: google.maps.LatLng
+  ) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+      browserHasGeolocation
+        ? "Error: The Geolocation service failed."
+        : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(map);
+  }
 
   // Initialise overlay
   useEffect(() => {
@@ -57,6 +86,37 @@ const InteractiveMap: React.FC<InteractiveMapProps> = ({
           west: 174.779711,
         }
       ).setMap(map);
+      const infoWindow = new google.maps.InfoWindow();
+      const locationButton = document.createElement("button");
+
+      locationButton.textContent = "Click here to pin your location";
+      locationButton.classList.add("custom-map-control-button");
+      map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+
+      locationButton.addEventListener("click", () => {
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+            (position: GeolocationPosition) => {
+              const pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+              };
+
+              infoWindow.setPosition(pos);
+              infoWindow.setContent("You are here!");
+              infoWindow.open(map);
+              map.setCenter(pos);
+            },
+            () => {
+              handleLocationError(true, infoWindow, map.getCenter()!);
+            }
+          );
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter()!);
+        }
+      });
     }
   }, [map]);
 
@@ -68,20 +128,181 @@ const InteractiveMap: React.FC<InteractiveMapProps> = ({
     const polygons = plots.reduce((polygonMap, plot) => {
       const polygon = new google.maps.Polygon({
         paths: plot.coordinates,
-        strokeColor: "#428bca",
+        strokeColor: plotColour,
         strokeOpacity: 0.6,
         strokeWeight: 2,
-        fillColor: "#428bca",
+        fillColor: plotColour,
         fillOpacity: 0.2,
       });
       polygon.setMap(map ?? null);
+
+      const point = averageCoordinates(plot.coordinates);
+
+      const infowindow1 = new google.maps.InfoWindow({
+        zIndex: 2,
+        maxWidth: 250,
+      });
+      const infowindow = new google.maps.InfoWindow({
+        zIndex: 1,
+      });
+
+      const personId = plot.buried.map((person: IPerson) => person._id);
+      const personNames = plot.buried.map((person: IPerson) => person.fullName);
+      const personDateOfBirths = plot.buried.map((person: IPerson) =>
+        "<p></p>" + person.dateOfBirth
+          ? dateToString(person.dateOfBirth)
+          : "Unknown"
+      );
+      const personDateOfDeaths = plot.buried.map((person: IPerson) =>
+        "<p></p>" + person.dateOfDeath
+          ? dateToString(person.dateOfDeath)
+          : "Unknown"
+      );
+
+      polygon.addListener("mouseover", () => {
+        infowindow.setContent(
+          "<h2>" +
+            plot.registeredName +
+            " Plot #" +
+            plot.plotNumber +
+            "</h2>" +
+            "<b>" +
+            "<p>Number of People: " +
+            plot.buried.length +
+            "</p>" +
+            "</b>" +
+            "<div id='button'></div>" +
+            "<br></br>"
+        );
+        infowindow.setPosition(point);
+        infowindow.open(map);
+      });
+
+      google.maps.event.addListener(infowindow, "domready", () => {
+        const parentbtn = document.getElementById("button");
+        for (let i = 0; i < personNames.length; i++) {
+          const btn = document.createElement("button");
+          btn.id = String(i);
+          btn.innerHTML = personNames[i] + "&#8505";
+          if (parentbtn !== null) {
+            parentbtn.appendChild(btn);
+          }
+        }
+
+        parentbtn?.addEventListener("click", (e) => {
+          if (e.target !== null) {
+            const btn = e.target as Element;
+            const btnNumber = Number(btn.id);
+            const profileLink: string = "'profile/" + personId[btnNumber] + "'";
+            const aLink: string = "<a href=" + profileLink + ">";
+
+            const fetchPromise = fetch(`/api/person/${personId[btnNumber]}`);
+
+            fetchPromise.then((response) => {
+              const jsonPromise = response.json();
+              jsonPromise.then((data) => {
+                if (data.images[0] !== undefined) {
+                  const imageUrl = data.images[0].url;
+                  const personImageUrl: string =
+                    "<img src=" + imageUrl + " width='200' height='100'}>";
+                  infowindow1.setContent(
+                    "<p></p>" +
+                      "<h2>" +
+                      "<b>" +
+                      aLink +
+                      personNames[btnNumber] +
+                      "</a></b></h2>" +
+                      "<p></p>" +
+                      aLink +
+                      personImageUrl +
+                      "</img>" +
+                      "</a>" +
+                      "<p></p>" +
+                      "Plot: " +
+                      plot.registeredName +
+                      " #" +
+                      plot.plotNumber +
+                      "<p></p>" +
+                      "Date of birth: " +
+                      personDateOfBirths[btnNumber] +
+                      "<p></p>" +
+                      "Date of death: " +
+                      personDateOfDeaths[btnNumber] +
+                      "<p></p>" +
+                      "<button id='back'>&#8592</button>"
+                  );
+                } else {
+                  const personImageUrl =
+                    "<img src='images/default-dp.png' width='200' height='200'}>";
+                  infowindow1.setContent(
+                    "<p></p>" +
+                      "<h2>" +
+                      "<b>" +
+                      aLink +
+                      personNames[btnNumber] +
+                      "</a></b></h2>" +
+                      "<p></p>" +
+                      aLink +
+                      personImageUrl +
+                      "</img>" +
+                      "</a>" +
+                      "<p></p>" +
+                      "Plot: " +
+                      plot.registeredName +
+                      " #" +
+                      plot.plotNumber +
+                      "<p></p>" +
+                      "Date of birth: " +
+                      personDateOfBirths[btnNumber] +
+                      "<p></p>" +
+                      "Date of death: " +
+                      personDateOfDeaths[btnNumber] +
+                      "<p></p>" +
+                      "<button id='back'>&#8592</button>"
+                  );
+                }
+              });
+            });
+
+            infowindow1.setPosition(point);
+            infowindow1.open(map);
+          }
+        });
+      });
+
+      google.maps.event.addListener(infowindow1, "domready", () => {
+        const back = document.getElementById("back");
+        back?.addEventListener("click", () => {
+          infowindow1.close();
+        });
+      });
+
+      polygon.addListener("mouseout", () => {
+        infowindow.close();
+      });
+
       return polygonMap.set(plot.plotNumber, polygon);
     }, new Map<number, google.maps.Polygon>());
 
     setPolygonsByNumber(polygons);
 
     return () => polygons.forEach((polygon) => polygon.setMap(null));
-  }, [map, plots]);
+  }, [map, plots, plotColour]);
+
+  // Highlight anniversary graveyard plots
+  useEffect(() => {
+    const matchedPlots = getAnniversaryPlots(plots);
+    // console.log(matchedPlots)
+    if (matchedPlots) {
+      matchedPlots.forEach((plot) => {
+        // console.log(plot)
+        if (plot) {
+          const selectedPolygon = polygonsByNumber?.get(plot.plotNumber);
+          selectedPolygon?.setOptions({ fillColor: "#7A49FF" });
+        }
+      });
+    }
+  }, [plots, polygonsByNumber]);
 
   // Initialise click listeners
   useEffect(() => {
@@ -115,7 +336,6 @@ const InteractiveMap: React.FC<InteractiveMapProps> = ({
   }, [map, selectedPlot, selectedPlotMarker]);
 
   // Geolocation marker
-  const geoLocation = useGeolocation();
   const geolocationMarker = useMemo(
     () =>
       new google.maps.Marker({
