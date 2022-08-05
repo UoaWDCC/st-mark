15e1a78de327265cf65406282d3538d2ddf722f5
Michelle <mwan391@aucklanduni.ac.nz>
2022-08-05 21:50:52 +1200

    change date picker to work with month and day only

--- frontend/src/components/Directory/SearchBar/index.tsx
+++ frontend/src/components/Directory/SearchBar/index.tsx
@@ -3,22 +3,22 @@ import { Paper, InputBase, Tooltip, InputAdornment } from "@mui/material";
 import { Search as SearchIcon } from "@mui/icons-material";
 import styles from "./SearchBar.module.css";
 
-import TextField from "@mui/material/TextField";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
-import Dialog from "@mui/material/Dialog";
-
-import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { IDate } from "../../../types/schema";
 
-import { getDate, getMonth, getYear } from "date-fns";
+import { getDate, getMonth } from "date-fns";
 
 import IconButton from "@mui/material/IconButton";
-import AutorenewRoundedIcon from "@mui/icons-material/AutorenewRounded";
 import DateRangeIcon from "@mui/icons-material/DateRange";
-import Popover from "@mui/material/Popover";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+
+import Popper from '@mui/material/Popper';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+
+
+
+import { DateObject, Calendar } from "react-multi-date-picker"
+import type { Value } from "react-multi-date-picker"
+
+
 
 interface SearchBarProps {
   onSearchTermChange: (newValue: string) => void;
@@ -26,46 +26,36 @@ interface SearchBarProps {
 }
 
 const SearchBar: React.FC<SearchBarProps> = (props: SearchBarProps) => {
-  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
-    null
-  );
 
-  const handleOpenPopOver = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
 
-  const handleClosePopOver = () => {
-    setAnchorEl(null);
-  };
 
-  const openDatePicker = Boolean(anchorEl);
-  const id = openDatePicker ? "simple-popover" : undefined;
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLButtonElement>(null);
 
-  const dateTheme = createTheme({
-    palette: {
-      // mode: "dark"
-      // background: {
-      //   paper: '#e3f2fd'
-      // },
-    },
-  });
-
-  const { onSearchTermChange, onDeathDateChange } = props;
-
-  const [date, setDate] = React.useState<Date | null>(null);
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
   };
 
   const handleClose = () => {
-    setOpen(false);
+    setAnchorEl(null);
   };
 
-  console.log(date);
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popper' : undefined;
+
+  const { onSearchTermChange, onDeathDateChange } = props;
+
+  const [date, setDate] = React.useState<Value>(new Date());
+
+  if (date instanceof DateObject) {
+    console.log(date.toDate());
+
+  } else {
+    console.log("date not exist");
+
+  }
 
   return (
+
     <Paper className={styles.container}>
       <div className={styles.searchBar}>
         <InputBase
@@ -76,109 +66,62 @@ const SearchBar: React.FC<SearchBarProps> = (props: SearchBarProps) => {
           }}
           className={styles.searchInput}
           startAdornment={
-            <InputAdornment position="start">
-              <div>
+            <div>
+              <InputAdornment position="start">
                 <IconButton
-                  onClick={handleOpenPopOver}
+                  onClick={handleClick}
                   sx={{ p: "10px" }}
                   aria-label="menu"
                 >
                   <DateRangeIcon />
                 </IconButton>
-                <Popover
+
+                <Popper
                   id={id}
-                  open={openDatePicker}
+                  open={open}
                   anchorEl={anchorEl}
-                  onClose={handleClosePopOver}
-                  anchorOrigin={{
-                    vertical: "bottom",
-                    horizontal: "left",
-                  }}
-                  transformOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
                 >
-                  <LocalizationProvider dateAdapter={AdapterDateFns}>
-                    <ThemeProvider theme={dateTheme}>
-                      <DatePicker
-                        className="date-picker"
-                        // views={['year', 'month', 'day']}
-                        openTo="year"
-                        minDate={new Date(1800, 0, 1)}
-                        value={date}
-                        onChange={(newDateValue: Date | null) => {
-                          setDate(newDateValue);
-                          newDateValue
-                            ? onDeathDateChange({
-                                year: getYear(newDateValue),
-                                month: getMonth(newDateValue) + 1,
-                                day: getDate(newDateValue),
-                              })
-                            : onDeathDateChange({});
-                        }}
-                        renderInput={(params) => {
-                          console.log(params.inputProps);
-                          return (
-                            <TextField
-                              {...params}
-                              InputProps={{
-                                ...params.InputProps,
-                                endAdornment: (
-                                  <>
-                                    <InputAdornment position="end">
-                                      <IconButton
-                                        onClick={() => {
-                                          setDate(null);
-                                          onDeathDateChange({});
-                                        }}
-                                      >
-                                        <AutorenewRoundedIcon />
-                                      </IconButton>
-                                    </InputAdornment>
-                                    {params.InputProps?.endAdornment}
-                                  </>
-                                ),
-                              }}
-                            />
-                          );
-                        }}
-                      />
-                    </ThemeProvider>
-                  </LocalizationProvider>
-                </Popover>
-              </div>
-            </InputAdornment>
+                  <ClickAwayListener onClickAway={handleClose}>
+                    <Calendar
+                      hideYear={true}
+                      value={date}
+                      onChange={
+                        (v: DateObject) => {
+                          setDate(v)
+                          v ?
+                            onDeathDateChange({
+                              month: getMonth(v.toDate()) + 1,
+                              day: getDate(v.toDate())
+                            })
+                            : onDeathDateChange({})
+                          handleClose()
+                        }
+                      }
+                    >
+                      <button
+                        style={{ margin: "5px 0" }}
+                        onClick={() => {
+                          onDeathDateChange({})
+                          handleClose()
+                        }
+                        }
+                      >
+                        Reset
+                      </button>
+                    </Calendar>
+
+                  </ClickAwayListener>
+                </Popper>
+              </InputAdornment>
+            </div>
           }
         />
         <Tooltip title="Search">
-          <SearchIcon onClick={handleClickOpen} />
+          <SearchIcon />
         </Tooltip>
       </div>
-      <Dialog open={open} onClose={handleClose}>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <ThemeProvider theme={dateTheme}>
-            <StaticDatePicker
-              className="date-picker"
-              views={["year", "month", "day"]}
-              openTo="year"
-              value={date}
-              onChange={(newDateValue: Date | null) => {
-                setDate(newDateValue);
-                newDateValue
-                  ? onDeathDateChange({
-                      year: getYear(newDateValue),
-                      month: getMonth(newDateValue) + 1,
-                      day: getDate(newDateValue),
-                    })
-                  : onDeathDateChange({});
-              }}
-              renderInput={(params) => <TextField {...params} />}
-            />
-          </ThemeProvider>
-        </LocalizationProvider>
-      </Dialog>
-    </Paper>
+
+    </Paper >
   );
 };
 
